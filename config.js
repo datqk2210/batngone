616e712ba3045c05ebca3a4ce824e74c0c357b32
datqk2210 <85298921+datqk2210@users.noreply.github.com>
2021-06-03 21:20:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const CONFIG = {
     introTitle: 'Hey u!',
     introDesc: `T có một bất ngờ dành cho m :))
     Bấm vào đi !!!`,
-    btnIntro: 'Bấm vào đây',
+    btnIntro: 'Bấm vào đây này!',
     title: 'Phải chăng m đã thích tao :v ',
     desc: 'Phải chăng em đã say ngay từ lúc m thấy điều này :v ',
     btnYes: 'Đúng rồi t vẫn cứ thích a <33',
