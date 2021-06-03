5f2d4a98883de0c77a15376ca6235fb44315ffba
datqk2210 <85298921+datqk2210@users.noreply.github.com>
2021-06-03 21:19:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    introTitle: 'Hey u!',
+    introDesc: `T có một bất ngờ dành cho m :))
+    Bấm vào đi !!!`,
+    btnIntro: 'Bấm vào đây',
+    title: 'Phải chăng m đã thích tao :v ',
+    desc: 'Phải chăng em đã say ngay từ lúc m thấy điều này :v ',
+    btnYes: 'Đúng rồi t vẫn cứ thích a <33',
+    btnNo: 'Không, M có chắc là thế không',
+    question:'Chả hiểu sao t lại thích m nữa :)) <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    reply: 'm thích t thì đừng cố dối lòng nói gì khác nữa :3',
+    mess: 'T biết mà 🥰. Yêu m nhiều nhiều 😘',
+    messDesc: 'Bất ngờ chưa !.',
+    btnAccept: '<3',
+    messLink: 'http://fb.com/qk221020' 
 }
